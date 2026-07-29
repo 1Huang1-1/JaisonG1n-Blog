350e7e604f528c8244e8d16e20c31b6b6d057b76
hjj <2836453230@qq.com>
2026-07-29 12:00:52 +0800

    chore: update avatar and pio welcome message

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,7 +2,7 @@ import type { ProfileConfig } from "../types/config";
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "JaisonG1n",
 	bio: "Code, AI and Everything in Between",
 	typewriter: {
